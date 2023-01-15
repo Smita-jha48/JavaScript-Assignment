d547b5e92d024bf4fb2389f134f36f8c8c3929cb
Smita <Smita_Jha@mckinsey.com>
2023-01-16 01:12:53 +0530

    feat : function to check whether leap year or not

--- LeapYearChecker.js
+++ LeapYearChecker.js
@@ -1,4 +1,8 @@
 function isLeapYear(year){
    return ( (year%4 ==0 && year%100!=0) ? true : (year%100 == 0) ? true: false ); 
 }
+let isLeapYear_arrow = (year)=>{
+   return ( (year%4 ==0 && year%100!=0) ? true : (year%100 == 0) ? true: false ); 
+}
 console.log(isLeapYear(2003));
+console.log(isLeapYear_arrow(2004))
